77ebccec34884e63e121d01598aa38265b23901d
Nicolas <Nicolas.kaasch@live.de>
2023-04-10 19:39:50 +0200

    Data classes init

---
+++ Entityframework_MSSQL_Test/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entityframework_MSSQL_Test.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+}
---
+++ Entityframework_MSSQL_Test/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entityframework_MSSQL_Test.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public int Age { get; set; }
+        public int Balance { get; set; }
+
+        public List<Address> Addresses { get; set; }
+    }
+}
