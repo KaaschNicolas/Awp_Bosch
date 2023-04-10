8deb4b9677271454968da48378792c7fa6d9beb5
Nicolas <Nicolas.kaasch@live.de>
2023-04-10 20:10:20 +0200

    DB Context add code

--- Entityframework_MSSQL_Test/DataAccess/UserContext.cs
+++ Entityframework_MSSQL_Test/DataAccess/UserContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entityframework_MSSQL_Test.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,15 @@ namespace Entityframework_MSSQL_Test.DataAccess
 {
     public class UserContext : DbContext
     {
+        private readonly string _connectionString;
         public UserContext(DbContextOptions dbOptions) : base(dbOptions) { }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("connectionStringkommthierMashallah");
+        }
     }
 }
--- Entityframework_MSSQL_Test/Program.cs
+++ Entityframework_MSSQL_Test/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Entityframework_MSSQL_Test.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography.X509Certificates;
+
 Console.WriteLine("Hello, World!");
+
+UserContext dbContext = new(new DbContextOptions(true);
