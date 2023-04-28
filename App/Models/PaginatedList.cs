01bcd3c7db4d7dd85b31a3916668f143f2c9e322
Nicolas <Nicolas.kaasch@live.de>
2023-04-28 18:37:30 +0200

    init PaginatedList + init StorageLocationPaginationVM

---
+++ App/Models/PaginatedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Models;
+public class PaginatedList<T> : List<T>
+{
+    public int PageIndex
+    {
+        get; private set;
+    }
+
+    public int PageCount
+    {
+        get; private set;
+    }
+
+    private PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+    {
+        PageIndex = pageIndex;
+        PageCount = (int)Math.Ceiling(count / (double)pageSize);
+        AddRange(items);
+    }
+
+    public static async Task<PaginatedList<T>> CreateAsync(
+    Response<List<T>> source,
+    int pageIndex,
+    int pageSize)
+    {
+        IQueryable<T> query = source.Data.AsQueryable();
+        int count = await query.CountAsync();
+        List<T> items = await query
+            .Skip((pageIndex - 1) * pageSize)
+            .Take(pageSize)
+            .ToListAsync();
+
+        return new PaginatedList<T>(items, count, pageIndex, pageSize);
+    }
+}
---
+++ App/ViewModels/StorageLocationPaginationViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.Models;
+using App.Core.Services.Interfaces;
+using App.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace App.ViewModels;
+public class StorageLocationPaginationViewModel : ObservableObject
+{
+    public StorageLocationPaginationViewModel(ICrudService<StorageLocation> crudService)
+    {
+        _crudService = crudService;
+    }
+
+    private readonly ICrudService<StorageLocation> _crudService;
+
+    private int _pageSize = 10;
+    private int _pageNumber;
+    private int _pageCount;
+    private List<StorageLocation> _storageLocations;
+
+    public int PageNumber
+    {
+        get => _pageNumber;
+        private set => SetProperty(ref _pageNumber, value);
+    }
+
+    public int PageCount
+    {
+        get => _pageCount;
+        private set => SetProperty(ref _pageCount, value);
+    }
+
+    public List<StorageLocation> StorageLocations
+    {
+        get => _storageLocations;
+        private set => SetProperty(ref _storageLocations, value);
+    }
+
+    private async Task GetStorageLocations(int pageIndex, int pageSize)
+    {
+        var storageLocations = await _crudService.GetAll();
+        if (storageLocations.Code == ResponseCode.Success)
+        {
+            PaginatedList<StorageLocation> storageLocationsPaginated = await PaginatedList<StorageLocation>.CreateAsync(
+                storageLocations,
+                pageIndex,
+                pageSize
+                );
+            PageNumber = storageLocationsPaginated.PageIndex;
+            PageCount = storageLocationsPaginated.PageCount;
+            StorageLocations = storageLocationsPaginated;
+        }
+    }
+}
