57fcb419e51ff65ebaf7fd81a6febbdc2d72b408
TK <t.kuehn@oneup-digital.com>
2023-05-02 22:35:01 +0200

    Added RequestedTheme Property -> used when switching themes

--- App/Services/DialogService.cs
+++ App/Services/DialogService.cs
@@ -1,4 +1,5 @@
-﻿using App.Contracts.Services;
+﻿using System.Drawing;
+using App.Contracts.Services;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
@@ -11,24 +12,33 @@ namespace App.Services;
 public sealed class DialogService : IDialogService
 {
     /// <inheritdoc/>
+    private readonly FrameworkElement? rootElement = App.MainWindow.Content as FrameworkElement;
     public async Task<bool?> ConfirmDeleteDialogAsync(string title, string content, string confirmButtonText, string cancelButtonText)
     {
-        var dialog = new ContentDialog
-        {
-            Title = title,
-            Content = content,
-            PrimaryButtonText = confirmButtonText,
-            CloseButtonText = cancelButtonText,
-            XamlRoot = App.MainWindow.Content.XamlRoot
+        if (rootElement != null)
 
-        };
-        var result = await dialog.ShowAsync();
-
-        if (result == ContentDialogResult.None)
         {
-            return null;
+            var dialog = new ContentDialog
+            {
+                Title = title,
+                Content = content,
+                PrimaryButtonText = confirmButtonText,
+                DefaultButton = ContentDialogButton.Close,
+                RequestedTheme = rootElement.RequestedTheme,
+                CloseButtonText = cancelButtonText,
+                XamlRoot = rootElement.XamlRoot
+
+            };
+            var result = await dialog.ShowAsync();
+
+            if (result == ContentDialogResult.None)
+            {
+                return null;
+            }
+
+            return (result == ContentDialogResult.Primary);
         }
-
-        return (result == ContentDialogResult.Primary);
+        return null;
     }
+       
 }
