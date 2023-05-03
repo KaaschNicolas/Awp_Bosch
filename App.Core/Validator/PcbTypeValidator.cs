d857c3bb550e9d34101265711a4ddb026bb35b0c
sfa <samer.fares@dscsag.com>
2023-05-03 04:30:52 +0200

    Add Validation and Test Method

---
+++ App.Core/Validator/PcbTypeValidator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Core.Models;
+
+namespace App.Core.Validator;
+public class PcbTypeValidator
+{
+    private PcbType _pcbType;
+
+    public PcbTypeValidator(PcbType pcbType)
+    {
+        _pcbType = pcbType;
+    }
+
+    private List<string> Validate()
+    {
+        List<string> errors = new List<string>();
+
+        if(string.IsNullOrEmpty(_pcbType.PcbPartNumber) || _pcbType.PcbPartNumber.Length > 10)
+        {
+            errors.Add("Name darf nicht null sein oder 10 Zeichen überschreiten.");
+        }
+
+        if(_pcbType.Description == null)
+        {
+            errors.Add("Description darf nicht null sein.");
+        }
+
+        if (_pcbType.MaxTransfer <= 0)
+        {
+            errors.Add("MaxTransfer muss mindestens 1 betragen.");
+        }
+
+        return errors;
+    }
+
+    public bool IsValid()
+    {
+        if (Validate().Count == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public List<string> GetErrors()
+    {
+        return Validate();
+    }
+}
