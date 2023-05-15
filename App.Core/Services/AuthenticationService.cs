7faa4cdcb9809090f79c3d3cbea7bda985a6ad81
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-05-15 08:13:27 +0200

    BOS-186
    authentication by adusername

---
+++ App.Core/Services/AuthenticationService.cs
@@ -0,0 +1,41 @@
+﻿using App.Core.Models;
+using App.Core.Services.Interfaces;
+
+namespace App.Core.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private bool _isAuthenticated;
+        private User _currentUser;
+
+        private ICrudService<User> _crudService;
+        public AuthenticationService(ICrudService<User> crudService) {
+            _crudService = crudService;
+            authenticateUser();
+        }
+
+        public async void authenticateUser() {
+            _isAuthenticated = false;
+            var adUsername = Environment.UserName;
+            var result = await _crudService.GetAll();
+            var users = result.Data;
+            foreach (var user in users)
+            {
+                _isAuthenticated = String.Equals(adUsername, user.AdUsername);
+                if (_isAuthenticated) {
+                    _currentUser = user;
+                    break;
+                }
+            }
+        }
+        public bool isAuthenticated()
+        {
+            return _isAuthenticated;
+        }
+
+        public User currentUser()
+        {
+            return _currentUser;
+        }
+    }
+}
