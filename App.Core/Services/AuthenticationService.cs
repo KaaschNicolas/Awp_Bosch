5ec53f550df28d17d583c60e8de4f529f1afedb1
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-05-15 10:11:44 +0200

    update of Authentication Service

--- App.Core/Services/AuthenticationService.cs
+++ App.Core/Services/AuthenticationService.cs
@@ -1,40 +1,42 @@
-﻿using App.Core.Models;
+﻿using App.Core.DataAccess;
+using App.Core.Models;
 using App.Core.Services.Interfaces;
 
 namespace App.Core.Services
 {
     public class AuthenticationService : IAuthenticationService
     {
-        private bool _isAuthenticated;
+        private bool _isAuthenticated = false;
         private User _currentUser;
 
-        private ICrudService<User> _crudService;
-        public AuthenticationService(ICrudService<User> crudService) {
-            _crudService = crudService;
-            authenticateUser();
+        private BoschContext _boschContext;
+        public AuthenticationService(BoschContext boschContext) {
+            _boschContext = boschContext;
         }
-
-        public async void authenticateUser() {
-            _isAuthenticated = false;
+        private void authenticate() {
             var adUsername = Environment.UserName;
-            var result = await _crudService.GetAll();
-            var users = result.Data;
-            foreach (var user in users)
-            {
-                _isAuthenticated = String.Equals(adUsername, user.AdUsername);
-                if (_isAuthenticated) {
-                    _currentUser = user;
-                    break;
-                }
+            var result = _boschContext.Users.Where(u => u.AdUsername.Equals(adUsername)).ToList();
+            if (result.Count != 0 && result[0] != null) { 
+                _isAuthenticated = true;
+                _currentUser = result[0];
             }
         }
+
         public bool isAuthenticated()
         {
+            if (_currentUser == null)
+            {
+               authenticate();
+            }
             return _isAuthenticated;
         }
 
         public User currentUser()
         {
+            if (_currentUser == null)
+            {
+               authenticate();
+            }
             return _currentUser;
         }
     }
