14c0d40029009cded5d44b3fc4e3448a104daff0
Nicolas <Nicolas.kaasch@live.de>
2023-05-16 14:52:12 +0200

    refactor PcbDataService

---
+++ App.Core/Services/Interfaces/IPcbDataService.cs
@@ -0,0 +1,13 @@
+﻿using App.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Core.Services.Interfaces
+{
+    public interface IPcbDataService<T> : ICrudService<T> where T : Pcb
+    {
+    }
+}
--- App.Core/Services/PcbDataService.cs
+++ App.Core/Services/PcbDataService.cs
@@ -10,7 +10,7 @@ using App.Core.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace App.Core.Services;
-public class PcbDataService<T> : CrudServiceBase<T>, ICrudService<T> where T : Pcb
+public class PcbDataService<T> : CrudServiceBase<T>, IPcbDataService<T> where T : Pcb
 {
     public PcbDataService(BoschContext boschContext, LoggingService loggingService) : base(boschContext, loggingService) { }
 
