bddd137ef88994b72e0d553f9c30348913088def
TK <t.kuehn@oneup-digital.com>
2023-06-04 04:39:43 +0200

    removed required

--- App.Core/Models/Diagnose.cs
+++ App.Core/Models/Diagnose.cs
@@ -1,17 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Core.Models
 {
     public class Diagnose : BaseEntity
     {
-        [Required]
+
         [Column(TypeName = "nvarchar(650)")]
         public string Name
         {
