746b51823918ea99f4f530784cb0fb6512fbf3bb
Nicolas <Nicolas.kaasch@live.de>
2023-06-06 18:07:08 +0200

    bar plot orientation fix

--- App/ViewModels/DwellTimeEvaluationViewModel.cs
+++ App/ViewModels/DwellTimeEvaluationViewModel.cs
@@ -18,44 +18,46 @@ namespace App.ViewModels
         [ObservableProperty]
         private PlotModel _dwellTimeBarPlot;
 
-        public DwellTimeEvaluationViewModel(ITransferDataService<Transfer> transferDataService)
+        public DwellTimeEvaluationViewModel(ITransferDataService<Transfer> transferDataService, IStorageLocationDataService<StorageLocation> storageLocationDataService)
         {
             _transferDataService = transferDataService;
+            _storageLocationDataService = storageLocationDataService;
             InitializeBarPlot();    
         }
 
         private ITransferDataService<Transfer> _transferDataService;
+        private IStorageLocationDataService<StorageLocation> _storageLocationDataService;
 
         private async Task InitializeBarPlot()
         {
             DwellTimeBarPlot = new PlotModel();
             DwellTimeBarPlot.PlotAreaBorderColor = OxyColors.Transparent;
-            
+            DwellTimeBarPlot = await ColumnSeries();
 
-            var barSeries = new BarSeries()
-            {
-                ItemsSource = await GenerateBarItems(),
-                LabelPlacement = LabelPlacement.Outside,
-                TextColor = OxyColors.WhiteSmoke
-            };
-            barSeries.IsStacked = false;
-            
-            DwellTimeBarPlot.Series.Add(barSeries);
+            //var barSeries = new HistogramSeries()
+            //{
+            //    ItemsSource = await GenerateBarItems(),
+            //    LabelPlacement = LabelPlacement.Outside,
+            //    TextColor = OxyColors.WhiteSmoke
+            //};
+
+            //DwellTimeBarPlot.Series.Add(barSeries);
+
+            ////DwellTimeBarPlot.Axes.Add(new CategoryAxis
+            ////{
+            ////    Position = AxisPosition.Bottom,
+            ////    Key = "StorageLocation"
+            ////});
 
             //DwellTimeBarPlot.Axes.Add(new CategoryAxis
             //{
             //    Position = AxisPosition.Bottom,
-            //    Key = "StorageLocation"
+            //    Key = "DwellTime",
             //});
-            DwellTimeBarPlot.Axes.Add(new CategoryAxis
-            {
-                Position = AxisPosition.Left,
-                Key = "DwellTime"
-            });
-            DwellTimeBarPlot.Axes.Add(new LinearAxis() { Position = AxisPosition.Bottom, MinimumPadding = 0, AbsoluteMinimum = 0 });
+            //DwellTimeBarPlot.Axes.Add(new LinearAxis() { Position = AxisPosition.Left, MinimumPadding = 0, AbsoluteMinimum = 0 });
         }
 
-        private async Task<List<BarItem>> GenerateBarItems()
+        private async Task<List<HistogramItem>> GenerateBarItems()
         {
             var transfer = await _transferDataService.GetAll();
 
@@ -64,21 +66,45 @@ namespace App.ViewModels
                 var q = from x in transfer.Data
                         group x by new { x.StorageLocationId, CreatedAt = x.CreatedDate } into g
                         select new TransferDto(g.Key.StorageLocationId, g.Key.CreatedAt);
-                List<BarItem> barItems = new();
+                List<HistogramItem> barItems = new();
                 List<int> distinctStorageLocationIds = new();
                 foreach(var item in q)
                 {
                     if (!distinctStorageLocationIds.Contains(item.StorageLocationId))
                     {
                         distinctStorageLocationIds.Add(item.StorageLocationId);
-                        barItems.Add(new BarItem(value: 1));
+                        barItems.Add(new HistogramItem(0, 1, 2, item.StorageLocationId));
                     }
                 }
-
                 
                 return barItems;
             }
             return null;
+        }
+
+        public async Task<PlotModel> ColumnSeries()
+        {
+            var model = new PlotModel();
+
+            // specify axis keys
+            var barSeries = new BarSeries { XAxisKey = "Value", YAxisKey = "Category" };
+            barSeries.Items.Add(new BarItem { Value = 10 });
+            barSeries.Items.Add(new BarItem { Value = 12 });
+            barSeries.Items.Add(new BarItem { Value = 16 });
+            model.Series.Add(barSeries);
+
+            // specify key and position
+            var categoryAxis = new CategoryAxis { Position = AxisPosition.Bottom, Key = "Category" };
+            var storageLocation = await _storageLocationDataService.GetAll();
+            storageLocation.Data.ForEach(x => categoryAxis.Labels.Add(x.StorageName));
+            
+            model.Axes.Add(categoryAxis);
+
+            // specify key and position
+            var valueAxis = new LinearAxis { Position = AxisPosition.Left, Key = "Value" };
+            model.Axes.Add(valueAxis);
+
+            return model;
         }
 
         private record TransferDto(int StorageLocationId, DateTime CreatedAt);
