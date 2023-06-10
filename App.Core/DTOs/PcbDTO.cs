ff4bfbdfc67d5f3a6377916364de16db3d76af04
TK <t.kuehn@oneup-digital.com>
2023-06-10 15:33:47 +0200

    added

---
+++ App.Core/DTOs/PcbDTO.cs
@@ -0,0 +1,20 @@
+﻿using App.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Core.DTOs;
+
+[Keyless]
+public class PcbDTO
+{
+    public int PcbId { get; set; }
+
+    public Pcb Pcb { get; set; }
+
+    public string StorageName { get; set; }
+
+    public int DwellTimeStatus { get; set; }
+
+    public PcbDTO() { }
+}
+
+
