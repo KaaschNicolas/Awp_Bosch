137e93f8082acdac389f4486ff7d706f9be7fb23
fasa1016 <fasa1016@h-ka.de>
2023-06-18 13:31:07 +0200

    remove old classes

--- App/Services/PcbToPrintPageService/IPcbToPrintPageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using App.Core.Models;
-using Microsoft.UI.Xaml.Controls;
-namespace App.Services.PcbToPrintPageService
-{
-    public interface IPcbToPrintPageService
-    {
-        Page ConvertByPcb(Pcb pcb);
-    }
-}
