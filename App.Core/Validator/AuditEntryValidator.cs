bb572114dc189c594e0dd55ce238fcbba055603c
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-25 03:26:27 +0200

    Update AuditEntryValidator.cs

--- App.Core/Validator/AuditEntryValidator.cs
+++ App.Core/Validator/AuditEntryValidator.cs
@@ -1,46 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using App.Core.Models;
+﻿using App.Core.Models;
 
-namespace App.Core.Validator;
-public class AuditEntryValidator
+namespace App.Core.Validator
 {
-    private AuditEntry _auditEntry;
-    public AuditEntryValidator(AuditEntry auditEntry)
+    // Eine Klasse zur Validierung von AuditEntry-Objekten (Logging Objekte)
+    public class AuditEntryValidator
     {
-        _auditEntry = auditEntry;
-    }
-    private List<string> Validate()
-    {
-        List<string> errors = new List<string>();
-        
-        if(string.IsNullOrEmpty(_auditEntry.Message) || _auditEntry.Message.Length > 250)
+        private AuditEntry _auditEntry;
+
+        // Der Konstruktor der Klasse, der das zu überprüfende AuditEntry erhält
+        public AuditEntryValidator(AuditEntry auditEntry)
         {
-            errors.Add("Nachricht darf nicht leer sein oder 250 Zeichen überschreiten.");
+            _auditEntry = auditEntry;
         }
-        if(_auditEntry.User == null)
+
+        private List<string> Validate()
         {
-            errors.Add("Nutzer darf nicht null sein.");
+            List<string> errors = new List<string>();
+
+            // Überprüfung, ob die Nachricht des AuditEntrys leer ist oder mehr als 250 Zeichen enthält
+            if (string.IsNullOrEmpty(_auditEntry.Message) || _auditEntry.Message.Length > 250)
+            {
+                errors.Add("Nachricht darf nicht leer sein oder 250 Zeichen überschreiten.");
+            }
+
+            // Überprüfung, ob der Nutzer des AuditEntry null ist
+            if (_auditEntry.User == null)
+            {
+                errors.Add("Nutzer darf nicht null sein.");
+            }
+
+            // Überprüfung, ob das Level des AuditEntrys mehr als 20 Zeichen enthält
+            if (_auditEntry.Level.Length > 20)
+            {
+                errors.Add("Level darf nicht 20 Zeichen überschreiten.");
+            }
+
+            return errors; // Rückgabe der Liste der Validierungsfehler
         }
-        if (_auditEntry.Level.Length > 20)
+
+        // Eine Methode zur Überprüfung der Validität des AuditEntry
+        public bool IsValid()
         {
-            errors.Add("Level darf nicht 20 Zeichen überschreiten.");
+            // Überprüfung, ob die Anzahl der Validierungsfehler 0 ist
+            if (Validate().Count == 0)
+            {
+                return true; // Der AuditEntry ist valide
+            }
+
+            return false; // Es sind Validierungsfehler vorhanden
         }
-        return errors;
-    }
-    public bool IsValid()
-    {
-        if (Validate().Count == 0)
+
+        // Eine Methode zur Rückgabe der Liste der Validierungsfehler
+        public List<string> GetErrors()
         {
-            return true;
+            return Validate();
         }
-        return false;
-    }
-    public List<string> GetErrors()
-    {
-        return Validate();
     }
 }
+
