b00c8f68c840a4313a274e2d7cf9c43deaa0a5c4
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-29 16:27:52 +0200

    Update DashboardStorageLocationDTO.cs

--- App.Core/DTOs/DashboardStorageLocationDTO.cs
+++ App.Core/DTOs/DashboardStorageLocationDTO.cs
@@ -1,26 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.Core.DTOs
 {
+    // Das Data Transfer Object (DTO) "DashboardStorageLocationDTO" repräsentiert ein Objekt für das Dashboard mit Informationen zu einem Lagerort.
+    // Es handelt sich um eine Keyless-Entität, da sie nicht über einen primären Schlüssel verfügt.
     [Keyless]
     public class DashboardStorageLocationDTO
     {
+        // Name des Lagerorts
         public string StorageName { get; set; }
+        // Anzahl der PCBs
         public int CountPcbs { get; set; }
+        // Anzahl der PCBs mit grünem Status
         public int CountGreen { get; set; }
+        // Anzahl der PCBs mit gelbem Status
         public int CountYellow { get; set; }
+        // Anzahl der PCBs mit rotem Status
         public int CountRed { get; set; }
 
+        // Anzahl der PCBs mit rotem Status
         [NotMapped]
         public int Percentage { get; set; }
 
+        // Wert
         [NotMapped]
         public string Number { get; set; }
     }
