2b0bb95b683600e0bf9608d95878b59e54aac946
Daniel Schleise <102793815+scda1092@users.noreply.github.com>
2023-06-29 16:44:29 +0200

    Update DashboardPcbTypeDTO.cs

--- App.Core/DTOs/DashboardPcbTypeDTO.cs
+++ App.Core/DTOs/DashboardPcbTypeDTO.cs
@@ -1,20 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace App.Core.DTOs
 {
+    // Das Data Transfer Object (DTO) "DashboardPcbTypeDTO" repräsentiert ein Objekt für das Dashboard mit Informationen zu einem PCB-Typ.
+    // Die Klasse enthält Eigenschaften zur Anzahl der PCBs, zur PCB-Sachnummer sowie zu zusätzlichen Informationen für das Dashboard, wie den Wert und Prozentualen Anteil.
     public class DashboardPcbTypeDTO
     {
+        // Anzahl der PCBs
         public int Count { get; set; }
+        // Sachnummer des PCBs
         public string PcbPartNumber { get; set; }
 
+        // Wert
         [NotMapped]
         public string Number { get; set; }
 
+        // Prozentualer Anteil
         [NotMapped]
         public int Percentage { get; set; }
     }
