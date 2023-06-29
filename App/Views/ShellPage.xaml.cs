d64b8dd1b513e2baa64eddea54788ba2147aa277
Theo Pötzl <poth1011@h-ka.de>
2023-06-29 23:37:56 +0200

    WindowIcon.ico durch PfeilIcon.ico ersetzt zum Änderung des Applikation Icons.

--- App/Views/ShellPage.xaml.cs
+++ App/Views/ShellPage.xaml.cs
@@ -35,7 +35,7 @@ public sealed partial class ShellPage : Page
         ViewModel.NavigationViewService.Initialize(NavigationViewControl);
         ViewModel.InfoBarService.InfoBar = MessageBar;
 
-        // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.
+        // TODO: Set the title bar icon by updating /Assets/WindowIcon.ico.-done
         // A custom title bar is required for full window theme and Mica support.
         // https://docs.microsoft.com/windows/apps/develop/title-bar?tabs=winui3#full-customization
         App.MainWindow.ExtendsContentIntoTitleBar = true;
